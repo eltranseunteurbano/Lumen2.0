da7ae2b34d0c80e5051e42946cf8b8d3e54c23eb
Cristian Salguero <david_salinasx10@hotmail.com>
2019-11-15 10:53:31 -0500

    Paso de paginas

--- src/components/WelcomeInformation/WelcomeInformation.tsx
+++ src/components/WelcomeInformation/WelcomeInformation.tsx
@@ -10,21 +10,18 @@ import * as Routes from '../../constants/Routes';
 import UserContext from '../../hooks/UserContext';
 import IconLumen from '../../icons/IconLumen/IconLumen';
 
-
-
-
-
 const WelcomeInformation = () => {
 
-    const useUser = React.useContext(UserContext);
+    var useUser = React.useContext(UserContext);
 
     const [update, setUpdate] = useState(false);
 
     const setType = (type: string) => {
         useUser.type = type;
+        useUser.update("update", !update);
         setUpdate(!update);
     }
- 
+
     const renderFromuser = (user: string) => {
         switch (user) {
             case '':
@@ -54,7 +51,7 @@ const WelcomeInformation = () => {
                 return (
                     <section className="WelcomeInformation appear">
                         <div className="WelcomeInformation__icons">
-                            <button type="button" className="WelcomeInformation__icons__item" onClick={() => setType("") }> <FontAwesomeIcon icon={faAngleLeft} /> </button>
+                            <button type="button" className="WelcomeInformation__icons__item" onClick={() => setType("")}> <FontAwesomeIcon icon={faAngleLeft} /> </button>
                         </div>
                         <h1 className="WelcomeInformation__title">¡Bienvenido!</h1>
 
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './Home.scss';
 
 import IlustrationInformation from '../../components/ilustrationInformation/IlustrationInformation';
 import WelcomeInformation from '../../components/WelcomeInformation/WelcomeInformation';
+import UserContext from '../../hooks/UserContext';
 
 const Home = () => {
+
+    const useUser = useContext(UserContext);
+    const [update, setUpdate] = useState(false);
+
+    
+
+    useEffect(()=>{
+        useUser.addUpdate("update", setUpdate, update);
+    }, []);
 
     return(
         <section className="Home">
