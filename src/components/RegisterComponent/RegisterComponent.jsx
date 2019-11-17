52f1b267f7cfbf802091bbeb01b6c8acbfe65d9f
Jaime Burbano <burbanojaime98@gmail.com>
2019-11-16 22:40:23 -0500

    pasos terminados

--- src/components/RegisterComponent/RegisterComponent.jsx
+++ src/components/RegisterComponent/RegisterComponent.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './RegisterComponent.scss';
 
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
+
 const RegisterComponent = (props) => {
     console.log(props.step);
+
+    const [ showModal, setShowModal ] = React.useState(false);
+
+    const finalizar = () => {
+        setShowModal(true);
+    }
+
+    const closeFinalizar = () => {
+        setShowModal(false);
+    }
 
     const renderStep = (number) => {
 
@@ -100,8 +114,44 @@ const RegisterComponent = (props) => {
             
         case 3:
             return(
-                <>
-                </>
+                <article className="RegisterComponent_three appear">
+                        <p className="RegisterComponent_three__txt">
+                            Crea una contraseña de al menos <strong>4 números</strong> para proteger tus datos personales.
+                        </p>
+                        <p className="RegisterComponent_three__txt">
+                            Con esta podrás acceder a Lumen y ver el progreso de tu proyecto.
+                        </p>
+                    
+                    <div className="RegisterComponent_three__bottom">
+                        <label className="RegisterComponent_three__bottom__inputs__label">
+                            <p className="RegisterComponent_three__bottom__inputs__label__txt">Contraseña</p>
+                            <input type="password" name="contrasena"/>
+                        </label>
+                        
+                        <label className="RegisterComponent_three__bottom__inputs__label">
+                            <p className="RegisterComponent_three__bottom__inputs__label__txt">Confirmar Contraseña</p>
+                            <input type="password" name="confirmarcontrasena"/>
+                        </label>
+                    </div>
+
+                    <div className="RegisterComponent_two__btns">
+                        <button type="button" className="RegisterComponent_three__btns__btn">Atrás</button>
+                        <button type="button" className="RegisterComponent_three__btns__btn" onClick={finalizar}>Finalizar</button>
+                    </div>
+
+                    <ReactModal
+                        isOpen={ showModal }
+                        contentLabel="Confirmar Registro"
+                        className="Modal"
+                        overlayClassName="Overlay">
+                            <h1>¿Estás seguro que deseas finalizar el formulario?</h1>
+                            <p>Puedes devolverte y revisar que los datos sean los correctos antes de enviar el formulario de registro.</p>
+                            <div className="RegisterComponent_two__btns">
+                                <button type="button" className="RegisterComponent_three__btns__btn" onClick={closeFinalizar}>Revisar</button>
+                                <button type="button" className="RegisterComponent_three__btns__btn" >Finalizar</button>
+                            </div>
+                    </ReactModal>
+                </article>
             )
 
         default:
